3831df5b3565789781f34be7c4a1b3692cf530da
nullcy0 <141303391+nullcy0@users.noreply.github.com>
2024-01-31 02:44:30 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Instal Git
+apt-get update
+apt-get install -y git
+
+# Instal build-essential
+apt-get install -y build-essential
+
+# Instal cmake
+apt-get install -y cmake
+
+# Instal libuv1-dev
+apt-get install -y libuv1-dev
+
+# Instal libssl-dev
+apt-get install -y libssl-dev
+
+# Instal libhwloc-dev
+apt-get install -y libhwloc-dev
